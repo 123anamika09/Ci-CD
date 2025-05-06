70082fdd292a2ccfd6712551758377b84ead45b6
Anamika kumari <116051224+123anamika09@users.noreply.github.com>
2025-05-06 11:54:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        if (this.getAttribute('href').startsWith('#')) {
+            e.preventDefault();
+            const target = document.querySelector(this.getAttribute('href'));
+            target.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
+
